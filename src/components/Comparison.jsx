0edcd6797c70f4518fe3dcd8dcf4c31c89f0cbc1
JhondhelPago <jhondhelpago2307@gmail.com>
2024-09-30 15:33:54 +0800

    result json from the django server

--- src/components/Comparison.jsx
+++ src/components/Comparison.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import styles from '../style';
 import RedEmphasize from './RedHighlight';
 
 const Comparison = () => {
+
+  const [result, setResult] = useState(null)
 
   //const originalEssayString = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque euismod dolor sit amet tellus condimentum, vel vulputate magna vehicula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Etiam at quam vitae tortor volutpat commodo. Integer lacinia dictum massa, nec viverra lectus aliquet et. Mauris a sollicitudin turpis. Cras tincidunt, arcu et ultricies cursus, felis libero dictum risus, nec ultricies libero arcu eget arcu. Nam vel arcu nec urna dapibus scelerisque.'
 
@@ -12,7 +14,7 @@ const Comparison = () => {
 
   const originalEssayString = 'Education plays a crucial role in the developement of individuals and societies. It is a tool that equips people with knowledge, skills, and values that are essential for personal and professional growth. In today’s rapidly changing world, having a good education is more important than ever. Firstly, education helps people to think critically. It teaches students how to analyse information, make informed decisions, and solve problems effectively. In an age where misinformation is rampant, the ability to think critically is invaluable. Students who are educated can discern between reliable and unreliable sources, which is vital for their future success. Moreover, education fosters social cohesion. Schools are often where children learn to interact with others, make friends, and develop social skills. However, not everyone have access to quality education, which can lead to inequality in society. This disparity in education can perpetuate cycles of poverty and limit opportunities for many individuals. Furthermore, the economic impact of education cannot be overstated. A well-educated workforce is essential for economic growth and innovation. Companies tend to prefer hiring individuals with higher levels of education, which often leads to better job prospects and higher salaries. As a result, investing in education is also an investment in a country’s economy. In conclusion, the significance of education in today’s society is clear. It enhances critical thinking skills, promotes social cohesion, and drives economic growth. Therefore, it is imperitive that we continue to prioritize education for all individuals, regardless of their background.'
 
-  const ErrorEssay = ''
+  let ErrorEssay = ''
 
   const SuggestionEssay = ''
 
@@ -26,9 +28,14 @@ const Comparison = () => {
           'question1' : question1,
           'composition' : originalEssayString
         }
-      )
+      );
+      
+      setResult(response.data);
+      console.log(response);
 
-      console.log(response)
+      //get the spelling error and then highlight it to the original composition
+
+      ErrorEssay
 
     }catch(error){
       console.log('Error in Comparison component @ fetchEssayResults function');
