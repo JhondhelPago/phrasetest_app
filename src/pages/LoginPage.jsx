c989ed4638ad7ad1c34bdcebc779403dede8e195
JhondhelPago <jhondhelpago2307@gmail.com>
2024-10-13 19:18:42 +0800

    activate darkmode LoginPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,31 +4,33 @@ import {logo} from '../assets';
 
 import { Login, Button, } from "../components";
 
-const LoginPage = () => {
+const LoginPage = ({isDark, changeColorTheme}) => {
   return (
     <>
-    <section className='bg-primary dark:white w-full h-screen flex items-start'>
-        <div className='relative w-1/2 h-full  flex-col hidden lg:block'>
-        <div className={`${styles.paddingX} ${styles.flexCenter} `}>
-            <div className={`h-screen flex items-center justify-center`}>
-                <img src={logo} alt="phrasetest" className="w-[256px] h-[64px]"/>
-            </div>
-        </div>
-        </div>
-        <div className=' bg-white dark:primary w-full lg:w-1/2  h-full flex flex-col'>
-        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-            <div className={`${styles.boxWidth} text-white`}> 
-            <img src={logo} alt="phrasetest" className="w-[256px] h-[64px] mt-4"/>
-            <Login/>
-            <Button/>
-            </div>
-        </div>
-        </div>
-        
+        <div className={!isDark && 'dark'}>
+            <section className='bg-primary dark:white w-full h-screen flex items-start'>
+                <div className='relative w-1/2 h-full  flex-col hidden lg:block'>
+                <div className={`${styles.paddingX} ${styles.flexCenter} `}>
+                    <div className={`h-screen flex items-center justify-center`}>
+                        <img src={logo} alt="phrasetest" className="w-[256px] h-[64px]"/>
+                    </div>
+                </div>
+                </div>
+                <div className=' bg-white dark:bg-primary w-full lg:w-1/2  h-full flex flex-col'>
+                <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+                    <div className={`${styles.boxWidth} text-white`}> 
+                    <img src={logo} alt="phrasetest" className="w-[256px] h-[64px] mt-4"/>
+                    <Login isDark={isDark} changeColorTheme={changeColorTheme}/>
+                    <Button isDark={isDark} changeColorTheme={changeColorTheme}/>
+                    </div>
+                </div>
+                </div>
+                
 
-    </section>
+            </section>
+        </div>
 
-</>
+    </>
   )
 }
 
