20649aa70c5e1bfd1fcdaeb54645be8637300d64
JhondhelPago <jhondhelpago2307@gmail.com>
2024-10-27 09:48:56 +0800

    GetStartedButton to loginpage route

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,8 +1,18 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "../style";
 import { arrowUp } from "../assets";
 
-const GetStarted = () => (
-    <div className={`${styles.flexCenter} w-[256px] h-[100px] rounded-lg bg-blue-gradient p-[2px] cursor-pointer`}>
+const GetStarted = () => {
+
+  const navigate = useNavigate();
+
+  const routeToLogin = () => {
+    navigate('/loginpage');
+  }
+  
+  return (
+    <div className={`${styles.flexCenter} w-[256px] h-[100px] rounded-lg bg-blue-gradient p-[2px] cursor-pointer`} onClick={routeToLogin}>
       <div className={`${styles.flexCenter} flex-col bg-white dark:bg-primary w-[100%] h-[100%] rounded-lg`}>
       <div className={`${styles.flexStart} flex-row item-center `}>
         <p className="font-poppins font-medium text-[18px] leading-[23px]  text-gradient mr-1">
@@ -16,5 +26,7 @@ const GetStarted = () => (
       
     </div>
   )
+
+}
 
 export default GetStarted
