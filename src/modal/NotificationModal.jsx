e89429a1f169e308813b8826489278d2cf90022f
JR Solis <139000323+bibe1s@users.noreply.github.com>
2024-11-16 22:15:55 +0800

    minor

--- src/modal/NotificationModal.jsx
+++ src/modal/NotificationModal.jsx
@@ -12,17 +12,14 @@ const NotificationModal = () => {
   return (
     <div className="text-primary fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
             <div className="bg-gray-100 p-6 rounded-lg w-80 text-center">
-              <label className="block text-lg font-semibold mb-4">Please Provide Section Name to Create a New Section</label>
-              <textarea
-                className="w-full p-2 border border-gray-300 rounded-lg mb-4"
-                placeholder="Enter section name"
-              />
+              <label className="block text-lg font-semibold mb-4">Are you sure?</label>
+              
               <div className="flex justify-around">
                 <button className="px-4 py-2 bg-red-500 text-white rounded-lg" onClick={toggleNotificationModal}>
                   Cancel
                 </button>
                 <button className="px-4 py-2 bg-green-500 text-white rounded-lg">
-                  Save
+                  Confirm
                 </button>
               </div>
             </div>
