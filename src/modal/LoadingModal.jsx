70822c932748a100d2ef8ecee5bf987ea7207c89
JR Solis <139000323+bibe1s@users.noreply.github.com>
2024-11-16 23:13:55 +0800

    LoadingModal

---
+++ src/modal/LoadingModal.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { ClipLoader } from "react-spinners";
+
+const LoadingModal = () => {
+
+  
+
+  // const toggleNotificationModal = ({ toggleNotificationModal } ) => {
+    
+  // };
+
+  const CancelHadler = () => {
+    setShowNotificationModal(false);
+  }
+
+
+  return (
+    <div className="text-primary fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+            <div className="bg-gray-100 p-6 rounded-lg w-80 text-center">
+              <label className="block text-lg font-semibold mb-4">Loading Please Wait</label>
+              
+              <div className="flex justify-around">
+              <ClipLoader color="#4A90E2" size={50} /> {/* Circular Spinner */}
+              </div>
+            </div>
+          </div>
+  )
+}
+
+export default LoadingModal
--- src/modal/index.js
+++ src/modal/index.js
@@ -1,9 +1,10 @@
 import NotificationModal from "./NotificationModal";
 import AlertModal from "./AlertModal";
+import LoadingModal from "./LoadingModal";
 
 
 export {
     NotificationModal,
     AlertModal,
-
+    LoadingModal,
 };
